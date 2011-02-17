c701c1bab5fe0d821060c824870ad533e18243a3
mattray <matthewhray@gmail.com>
2011-02-17 08:52:51 -0600

    removed chef-solo support and files unused for basic nova support

--- chef-solo.rb
+++
@@ -1,3 +0,0 @@
-file_cache_path "/tmp/chef-solo-cache"
-cookbook_path File.expand_path(File.join(File.dirname(__FILE__), "cookbooks"))
-role_path File.expand_path(File.join(File.dirname(__FILE__), "roles"))
