87f7766efca50c06339fdcc889225c431f5f2fb3
mattray <matthewhray@gmail.com>
2011-05-17 22:53:33 -0500

    removed support server role

--- roles/nova-multi-controller.rb
+++ roles/nova-multi-controller.rb
@@ -2,7 +2,8 @@ name "nova-multi-controller"
 
 description "Installs requirements to run the Controller node in a Nova cluster"
 run_list(
-         "role[nova-support-server]",
+         "role[nova-mysql-server]",
+         "role[nova-rabbitmq-server]",
          "role[nova-head]",
          "role[nova-cloud-controller]",
          "role[nova-super-user-setup]"
