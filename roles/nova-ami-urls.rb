7fdf29f60e7f97556df578bcb9edb87eb7828629
mattray <matthewhray@gmail.com>
2011-05-20 23:24:26 -0500

    updated role diagrams

--- roles/nova-ami-urls.rb
+++ roles/nova-ami-urls.rb
@@ -3,9 +3,8 @@ description "Feed in a list URLs for AMIs to download"
 default_attributes(
                    "nova" => {
                      "images" => [
+                                  "http://192.168.11.7/ttylinux-uec-amd64-12.1_2.6.35-22_1.tar.gz",
                                   "http://192.168.11.7/ubuntu1010-UEC-localuser-image.tar.gz"
-#                                  "http://admin:8091/ubuntu_dvd/ami/ttylinux-uec-amd64-12.1_2.6.35-22_1.tar.gz",
-#                                  "http://admin:8091/ubuntu_dvd/ami/ubuntu1010-UEC-localuser-image.tar.gz"
                                  ]
                    }
                    )
