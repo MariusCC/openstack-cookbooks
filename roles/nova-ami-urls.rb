12341a1e623757fbbd3a6898ff9b501e62799f8a
mattray <matthewhray@gmail.com>
2011-05-24 09:50:41 -0500

    dropped the TTY image

--- roles/nova-ami-urls.rb
+++ roles/nova-ami-urls.rb
@@ -3,7 +3,6 @@ description "Feed in a list URLs for AMIs to download"
 default_attributes(
                    "nova" => {
                      "images" => [
-                                  "http://192.168.11.50/ttylinux-uec-amd64-12.1_2.6.35-22_1.tar.gz",
                                   "http://192.168.11.50/ubuntu1010-UEC-localuser-image.tar.gz"
                                  ]
                    }
