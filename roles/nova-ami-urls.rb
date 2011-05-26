cc0227f87ac920aa9c4ef288f0277ff749b9a431
mattray <matthewhray@gmail.com>
2011-05-26 15:05:20 -0500

    new url

--- roles/nova-ami-urls.rb
+++ roles/nova-ami-urls.rb
@@ -3,7 +3,7 @@ description "Feed in a list URLs for AMIs to download"
 default_attributes(
                    "nova" => {
                      "images" => [
-                                  "http://192.168.11.50/ubuntu1010-UEC-localuser-image.tar.gz"
+                                  "http://192.168.11.5/ubuntu1010-UEC-localuser-image.tar.gz"
                                  ]
                    }
                    )
