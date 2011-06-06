24cceb5995ecae62c914602a814a9875364a4d05
mattray <matthewhray@gmail.com>
2011-06-06 10:13:51 -0500

    removing unused roles

--- roles/nova-ami-urls.rb
+++
@@ -1,9 +0,0 @@
-name "nova-ami-urls"
-description "Feed in a list URLs for AMIs to download"
-default_attributes(
-                   "nova" => {
-                     "images" => [
-                                  "http://192.168.11.5/ubuntu1010-UEC-localuser-image.tar.gz"
-                                 ]
-                   }
-                   )
